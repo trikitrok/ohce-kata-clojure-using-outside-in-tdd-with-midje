a1414d6b37ad4e4f97b32c616c8fbd2fb148f7d3
trikitrok <trikitrok@gmail.com>
2016-06-28 19:30:09 +0200

    refactor: moved some fuctions to test-helpers namespace

--- test/ohce/unit_tests/ohce_test.clj
+++ test/ohce/unit_tests/ohce_test.clj
@@ -2,33 +2,7 @@
   (:require
     [midje.sweet :refer :all]
     [ohce.core :refer :all]
-    [ohce.notifications :as notifications]))
-
-(defn- register-call [func-keyword an-atom & args]
-  (let [args (if (nil? args) :no-args args)
-        calls (vec (func-keyword @an-atom))]
-    (swap! an-atom assoc func-keyword (conj calls args))))
-
-(defn- args-of-call
-  [func-keyword atom-keyword protocol-implementation]
-  (func-keyword @(atom-keyword protocol-implementation)))
-
-(defrecord FakeNotifier [notifications]
-  notifications/Notifier
-  (greet [_ greeting]
-    (register-call :greet notifications greeting))
-
-  (echo [_ reversed-phrase]
-    (register-call :echo notifications reversed-phrase))
-
-  (palindromes-rock [_]
-    (register-call :palindromes-rock notifications))
-
-  (bye-user [_ name]
-    (register-call :bye-user notifications name)))
-
-(defn fake-notifier []
-  (->FakeNotifier (atom {})))
+    [ohce.test-helpers :as test-helpers]))
 
 (unfinished select-greeting)
 (unfinished read-input)
@@ -39,19 +13,19 @@
   (fact
     "it greets the user"
 
-    (let [notifier (fake-notifier)]
+    (let [notifier (test-helpers/fake-notifier)]
 
       (ohce select-greeting notifier read-input ...username...) => irrelevant
       (provided
         (read-input) => "Stop!"
         (select-greeting ...username...) => ...greeting...)
 
-      (args-of-call :greet :notifications notifier) => [[...greeting...]]))
+      (test-helpers/args-of-call :greet :notifications notifier) => [[...greeting...]]))
 
   (fact
     "it reverses the user inputs"
 
-    (let [notifier (fake-notifier)]
+    (let [notifier (test-helpers/fake-notifier)]
 
       (ohce select-greeting notifier read-input ...username...) => irrelevant
 
@@ -59,12 +33,12 @@
         (select-greeting ...username...) => irrelevant
         (read-input) =streams=> ["hola" "lolo" "Stop!"])
 
-      (args-of-call :echo :notifications notifier) => [["aloh"] ["olol"]]))
+      (test-helpers/args-of-call :echo :notifications notifier) => [["aloh"] ["olol"]]))
 
   (fact
     "it reverses the user inputs that are not blank"
 
-    (let [notifier (fake-notifier)]
+    (let [notifier (test-helpers/fake-notifier)]
 
       (ohce select-greeting notifier read-input ...username...) => irrelevant
 
@@ -72,12 +46,12 @@
         (select-greeting ...username...) => irrelevant
         (read-input) =streams=> ["" "Stop!"])
 
-      (args-of-call :echo :notifications notifier) => nil))
+      (test-helpers/args-of-call :echo :notifications notifier) => nil))
 
   (fact
     "it identifies palindromes"
 
-    (let [notifier (fake-notifier)]
+    (let [notifier (test-helpers/fake-notifier)]
 
       (ohce select-greeting notifier read-input ...username...) => irrelevant
 
@@ -85,13 +59,13 @@
         (select-greeting ...username...) => irrelevant
         (read-input) =streams=> ["oto" "ana" "Stop!"])
 
-      (args-of-call :echo :notifications notifier) => [["oto"] ["ana"]]
-      (args-of-call :palindromes-rock :notifications notifier) => [:no-args :no-args]))
+      (test-helpers/args-of-call :echo :notifications notifier) => [["oto"] ["ana"]]
+      (test-helpers/args-of-call :palindromes-rock :notifications notifier) => [:no-args :no-args]))
 
   (fact
     "it knows when to stop"
 
-    (let [notifier (fake-notifier)]
+    (let [notifier (test-helpers/fake-notifier)]
 
       (ohce select-greeting notifier read-input ...username...) => irrelevant
 
@@ -99,4 +73,4 @@
         (select-greeting ...username...) => irrelevant
         (read-input) =streams=> ["Stop!"])
 
-      (args-of-call :bye-user :notifications notifier) => [[...username...]])))
+      (test-helpers/args-of-call :bye-user :notifications notifier) => [[...username...]])))
