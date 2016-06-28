e1f0ba27f0a7a2b66b15341a0f40661ea8ac82fd
trikitrok <trikitrok@gmail.com>
2016-06-28 15:33:10 +0200

    refactor: introducing some explaining helpers

--- src/ohce/core.clj
+++ src/ohce/core.clj
@@ -2,6 +2,12 @@
   (:require
     [ohce.notifications :as notifications]))
 
+(defn- reverse-str [input]
+  (apply str (reverse input)))
+
+(defn- palindrome? [input]
+  (= input (reverse-str input)))
+
 (defn ohce
   ([hour-fn read-input name])
 
@@ -11,8 +17,8 @@
     (let [input (read-input)]
       (when (not (clojure.string/blank? input))
         (do
-          (notifications/echo notifier (apply str (reverse input)))
-          (when (= input (apply str (reverse input)))
+          (notifications/echo notifier (reverse-str input))
+          (when (palindrome? input)
             (notifications/palindromes-rock notifier))
           ))
       )))
