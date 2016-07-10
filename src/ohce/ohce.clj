2998c81eb8f70c1af298f8823ceb50b362335e58
trikitrok <trikitrok@gmail.com>
2016-07-10 13:07:23 +0200

    refactor: extracted some helpers

--- src/ohce/ohce.clj
+++ src/ohce/ohce.clj
@@ -11,18 +11,22 @@
 (defn- should-stop? [input stop-word]
   (= input stop-word))
 
-(defn- process-inputs [notifier read-input stop-word]
+(defn- respond-to-input [notifier input]
+  (notifications/echo notifier (reverse-str input))
+  (when (palindrome? input)
+    (notifications/palindromes-rock notifier)))
+
+(defn- process-input [notifier input]
+  (when-not (clojure.string/blank? input)
+    (respond-to-input notifier input)))
+
+(defn- interact-with-user [notifier read-input stop-word]
   (loop [input (read-input)]
     (when-not (should-stop? input stop-word)
-      (do
-        (when-not (clojure.string/blank? input)
-          (do
-            (notifications/echo notifier (reverse-str input))
-            (when (palindrome? input)
-              (notifications/palindromes-rock notifier))))
-        (recur (read-input))))))
+      (do (process-input notifier input)
+          (recur (read-input))))))
 
 (defn ohce [select-greeting notifier read-input stop-word name]
   (notifications/greet notifier (select-greeting name))
-  (process-inputs notifier read-input stop-word)
+  (interact-with-user notifier read-input stop-word)
   (notifications/bye-user notifier name))
